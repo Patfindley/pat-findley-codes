47e3c9aa570505b168d3bcb8a4413c1be056bef0
Patrick <>
2021-06-23 16:44:22 -0400

    Add socials to nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import './Nav.css';
+import github_mark from '../../assets/GitHub-Mark-32px.png'
+import linkedin from '../../assets/linkedin-logo.png'
 
 const Nav = () => {
   return (
@@ -9,6 +11,10 @@ const Nav = () => {
               <Link to='/projects' className='link'><li>Projects</li></Link>
               <Link to='/contact' className='link'><li>Contact</li></Link>
           </ul>
+          <div className='nav-socials'>
+            <a href='https://github.com/Patfindley' target='_blank' rel="noreferrer"><img className='socials github' src={github_mark} alt='github' /></a>
+            <a href='https://www.linkedin.com/in/patfindley/' target='_blank' rel="noreferrer"><img className='socials linkedin' src={linkedin} alt='linkedin' /></a>
+          </div>
         </nav>
   )
 }
