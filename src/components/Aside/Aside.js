1ddcc129fe0762345525b8df806fdab42bb2df23
Patrick <>
2021-07-04 22:10:52 -0400

    Disinguish clickable links in asides

--- src/components/Aside/Aside.js
+++ src/components/Aside/Aside.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
 import './Aside.css'
 
 const Aside = ({ selectedProject }) => {
+  // useEffect(() => {
+  //   gsap.fromTo(
+  //     '.aside-link',
+  //     {
+  //       color: () => {
+  //         return `#df562a`;
+  //       },
+  //     },
+  //     {
+  //       color: 'black',
+  //       duration: 5.5,
+  //       repeat: -1,
+  //       ease: 'none',
+  //     }
+  //   );
+  // });
+
   if (selectedProject.id === 0) {
     return (
       <section className='aside-wrap'>
@@ -12,9 +30,9 @@ const Aside = ({ selectedProject }) => {
   return (
     <section className='aside-project-wrap'>
         <h1>{selectedProject.name}</h1>
-        <a href={selectedProject.gitHub} target='_blank' rel="noreferrer"> GitHub </a>
+        <a className='aside-link' href={selectedProject.gitHub} target='_blank' rel="noreferrer"> GitHub </a>
         {selectedProject.deploy.length > 1 && 
-        <a href={selectedProject.deploy} target='_blank' rel="noreferrer"> Deploy </a>}
+        <a className='aside-link' href={selectedProject.deploy} target='_blank' rel="noreferrer"> Deploy </a>}
         <h4 className='aside-project-info'><strong>Date:</strong> {selectedProject.date}</h4>
         <h4 className='aside-project-info'><strong>Technologies:</strong> {selectedProject.technology.join(', ')}</h4>
         
