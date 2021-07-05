ec704483f8ca7e28cc8b4f9b063f0ebc39b9320f
Patrick <>
2021-07-05 16:16:09 -0400

    Add deploy link for rick and morty app

--- src/assets/data.js
+++ src/assets/data.js
@@ -96,7 +96,7 @@ export const projectsData = [
     body: `“I’ll tell you how I feel about school, Jerry: It’s a waste of time. Bunch of people runnin’ around bumpin’ into each other, got a guy up front says, ‘Two plus two,’ and the people in the back say, ‘Four.’ Then the bell rings and they give you a carton of milk and a piece of paper that says you can go take a dump or somethin’. I mean, it’s not a place for smart people, Jerry. I know that’s not a popular opinion, but that’s my two cents on the issue.” — Rick`,
     technology:['JavaScript', 'HTML', 'CSS', 'React', 'React-Router', 'GSAP', 'Styled Components', 'Cypress'],
     gitHub: 'https://github.com/Patfindley/Dimension-34.5N',
-    deploy: '',
+    deploy: 'https://guarded-chamber-88069.herokuapp.com/',
     tags:['JS', 'JavaScript', 'React', 'html', 'css', 'API', 'html5', 'Rick and Morty', 'Rick & Morty', 'silly', 'gsap', 'wubba lubba dub dub', 'animation', 'solo', 'git', 'github', 'cypress']
   }
 ];
