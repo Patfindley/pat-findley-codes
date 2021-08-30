a1b77bc9b17e5ccf872476397d7a8caf09c3f26c
Patrick <patfindley@gmail.com>
2021-08-30 19:15:34 -0400

    Bring contact wrap into contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,9 +9,12 @@ const Contact = () => {
   const contactMessage = useRef()
   const [submitted, setSubmitted] = useState(false)
 
+  // useEffect(() => {
+  //   gsap.to('.wave-4', 20, {css: {right: '300px'}, ease: 'power1', yoyo: 'true', repeat: '-1'})
+  // }, [])
   useEffect(() => {
-    gsap.to('.wave-4', 20, {css: {right: '300px'}, ease: 'power1', yoyo: 'true', repeat: '-1'})
-
+    gsap.to('.contact-wave', 30, {css: {right: '275px'}, ease: 'power1', yoyo: 'true', repeat: '-1'})
+    gsap.to('.contact-wave', 1.25, {css: {top: '70px'}, ease: 'bounce', yoyo: 'true', repeat: '-1'})
   }, [])
   
   const submitForm = (e) => {
@@ -44,14 +47,18 @@ const Contact = () => {
 } 
 
   return (
+    <>
+      <img className='contact-wave'src={wave4} alt='wave' />
     <div className='form-container'>
-      <img className='wave-4'src={wave4} alt='wave' />
+      <div className='contact-hello-wrap'>
+        <h1 className='contact-hello'> SAY HELLO! </h1>
+      </div>
       <form className='contact-form' action='https://docs.google.com/forms/u/0/d/e/1FAIpQLSed7xFAkBBdilezfZe5ek5uCfr1BLEKcrlloZpu-sE0boxrbQ/formResponse' method='POST'  
       onSubmit={(e) => {submitForm(e)}}
       >
         <label className='form-field label' for='name-input'>Your Name</label>
         <input 
-          className='form-field input'
+          className='form-name form-field input'
           for='name-input' 
           type='text' 
           ref={contactName} 
@@ -71,7 +78,7 @@ const Contact = () => {
         </input>
         <label className='form-field label' for='message-input'>Say Hello!</label>
         <input 
-          className='form-field input'
+          className='form-message form-field input'
           for='message-input' 
           type='text' 
           ref={contactMessage} 
@@ -82,6 +89,7 @@ const Contact = () => {
         <button className='submit-button' type='submit' >Submit</button>
       </form>
     </div>
+    </>
   )
 }
 
