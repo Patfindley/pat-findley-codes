b70d862cd9f1d79448ff4de3d4add75f6cf06df7
Patrick <patfindley@gmail.com>
2021-10-18 13:54:53 -0400

    Refactor into styled component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,21 +1,51 @@
 import React, { useRef } from 'react'
-import './SearchBar.css'
+import styled from 'styled-components'
+
+const Wrap = styled.div`
+  width: 22%;
+  display: flex;
+  justify-content: center;
+  z-index: 2;
+`
+
+const SearchForm = styled.form`
+  text-align: center;
+  width: 100%;
+  height: 50px;
+  & input:focus, textarea:focus {
+    outline-color: ${({ theme }) => theme.colors.waveFive};
+  }
+`
+
+const Input = styled.input`
+  width: 50%;
+  height: 100%;
+  border-radius: 10px;
+  border: solid #c8ccd0 1px;
+  transition: all 800ms;
+  &:hover {
+    border-color: ${({ theme }) => theme.colors.waveOne};
+    box-shadow: 0 0 10px ${({ theme }) => theme.colors.waveOne};
+    width: 80%;
+    transition: box-shadow linear 0.2s, width 500ms ease;
+  }
+`
 
 const SearchBar = ({ filterProjects }) => {
   const searchValue = useRef()
 
     return (
-      <div className='search-wrap'>
-      <form className="search-form">
-        <input className="search-input"
+      <Wrap>
+      <SearchForm>
+        <Input
           type="text"
           placeholder="search topic"
           ref={searchValue}
           name="input"
           onChange={() => filterProjects(searchValue.current.value)}
         />
-      </form>
-      </div>
+      </SearchForm>
+      </Wrap>
     )
 }
 
